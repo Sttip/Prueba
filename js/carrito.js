3de7a898c7f0e2f05aeb1b9020db646aa8bd864e
Sttip <sttip5789@gmail.com>
2025-10-13 19:31:10 -0300

    Delete js directory

--- js/carrito.js
+++
@@ -1,8 +0,0 @@
-// js/carrito.js
-// Funcionalidad básica para los botones "Agregar al Carrito"
-document.querySelectorAll('.add-to-cart').forEach(button => {
-    button.addEventListener('click', function() {
-        const smoothieName = this.parentElement.querySelector('.smoothie-name').textContent;
-        alert(`¡${smoothieName} agregado al carrito!`);
-    });
-});
