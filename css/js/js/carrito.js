07c171371ef59f18b8b24b76decc85a0b063c5b5
Sttip <sttip5789@gmail.com>
2025-10-13 19:10:52 -0300

    Create carrito.js

---
+++ css/js/js/carrito.js
@@ -0,0 +1,9 @@
+// js/carrito.js
+// Archivo listo para futura funcionalidad del carrito
+console.log('Carrito.js cargado - Listo para expandir funcionalidad');
+
+// Aquí puedes agregar en el futuro:
+// - Manejo del carrito de compras
+// - LocalStorage
+// - Cálculo de totales
+// - etc.
