a8d270005b08776edbdfd1857d43290f2ede0fc8
Sttip <sttip5789@gmail.com>
2025-10-13 20:08:54 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-// src/js/main.js
-
-// Mensaje básico de carrito (puedes mejorarlo más adelante)
 document.querySelectorAll('.add-to-cart').forEach(button => {
     button.addEventListener('click', function() {
         const smoothieName = this.parentElement.querySelector('.smoothie-name').textContent;
